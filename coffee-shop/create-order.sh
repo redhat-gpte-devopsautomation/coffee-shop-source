df2ce39c1513e2de8e09ab8ffe6d40e77400c5f9
Wolfgang Kulhanek <wkulhane@redhat.com>
2021-03-23 11:20:03 -0400

    Updates

--- coffee-shop/create-order.sh
+++ coffee-shop/create-order.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eu
 
-curl http://localhost:8080/orders -XPOST -i -H 'Content-Type: application/json' -d '{"origin":"Colombia","type":"Espresso"}'
+curl http://$(oc get route coffee-shop --template='{{ .spec.host }}'/orders -XPOST -i -H 'Content-Type: application/json' -d '{"origin":"Colombia","type":"Espresso"}'
