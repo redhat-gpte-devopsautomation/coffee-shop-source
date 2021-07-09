b3bd2a6238cf559648a050cc3af8aa45183dc51c
varodrig <varodrig@redhat.com>
2021-07-09 16:12:18 -0400

    updating logging

--- coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/Health.java
+++ coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/Health.java
@@ -3,31 +3,22 @@ package com.sebastian_daschner.coffee_shop;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Readiness;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 import javax.enterprise.context.ApplicationScoped;
-//import io.quarkus.logging.json.runtime.LoggingJsonRecorder;
-//import java.util.logging.Logger;
-// import java.util.logging.Level;
 
 
 @Readiness
 @ApplicationScoped
 public class Health implements HealthCheck {
 
-	//LoggingJsonRecorder log = LoggingJsonRecorder.class.
-	// private static final Logger log = LoggerFactory.getLogger(HealthCheck.class);
-
-  // private static final Logger logger = Logger.getLogger(Health.class.getName());
-
-  java.util.logging.Logger logger =  java.util.logging.Logger.getLogger(this.getClass().getName());
+	private static final Logger log = LoggerFactory.getLogger(Health.class);
 
 	@Override
     public HealthCheckResponse call() {
-        logger.info("This is an info message");
-  //      logger.log(Level.INFO, "Test log of structured arg");
+        log.info("Calling HealthCheckResponse");
         return HealthCheckResponse.up("coffee-shop");
 
     }
