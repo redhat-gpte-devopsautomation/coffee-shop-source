fc44f686b2d1a57574812768fda439e7614c5f58
varodrig <varodrig@redhat.com>
2021-07-09 12:58:31 -0400

    adding logging

--- coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/Health.java
+++ coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/Health.java
@@ -3,20 +3,26 @@ package com.sebastian_daschner.coffee_shop;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Readiness;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 import javax.enterprise.context.ApplicationScoped;
+import io.quarkus.logging.json.runtime.LoggingJsonRecorder;
 
-import static io.quarkus.logging.json.structured.KeyValueStructuredArgument.*;
 
 
 @Readiness
 @ApplicationScoped
 public class Health implements HealthCheck {
 
-    @Override
+	//LoggingJsonRecorder log = LoggingJsonRecorder.class.
+	private static final Logger log = LoggerFactory.getLogger(HealthCheck.class);
+    
+	@Override
     public HealthCheckResponse call() {
+        log.info("Test log of structured arg");
         return HealthCheckResponse.up("coffee-shop");
-        log.info("Test log of structured arg", kv("from", "Healthz"));
 
     }
 
