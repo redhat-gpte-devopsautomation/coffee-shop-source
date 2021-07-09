3834e7c4b2bc0092071f633b87b2376618e2c052
Judd Maltin <judd@newgoliath.com>
2021-07-09 16:59:16 -0400

    try simple

--- coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/Health.java
+++ coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/Health.java
@@ -3,9 +3,10 @@ package com.sebastian_daschner.coffee_shop;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Readiness;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 
+import java.util.logging.Level;
 
 import javax.enterprise.context.ApplicationScoped;
 
@@ -14,11 +15,12 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class Health implements HealthCheck {
 
-	private static final Logger log = LoggerFactory.getLogger(Health.class);
+	// private static final Logger log = LoggerFactory.getLogger(Health.class);
+  private static final java.util.logging.Logger logger =  java.util.logging.Logger.getLogger(this.getClass().getName());
 
 	@Override
     public HealthCheckResponse call() {
-        log.info("Calling HealthCheckResponse");
+        logger.log("Calling HealthCheckResponse");
         return HealthCheckResponse.up("coffee-shop");
 
     }
