4ebca5b931612ba2aadc872b20f041e8faea0ba0
Judd Maltin <judd@newgoliath.com>
2021-07-09 14:58:25 -0400

    try simpler

--- coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/Health.java
+++ coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/Health.java
@@ -3,13 +3,13 @@ package com.sebastian_daschner.coffee_shop;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Readiness;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 
 import javax.enterprise.context.ApplicationScoped;
-import io.quarkus.logging.json.runtime.LoggingJsonRecorder;
-
+//import io.quarkus.logging.json.runtime.LoggingJsonRecorder;
+import java.util.logging
 
 
 @Readiness
@@ -17,11 +17,14 @@ import io.quarkus.logging.json.runtime.LoggingJsonRecorder;
 public class Health implements HealthCheck {
 
 	//LoggingJsonRecorder log = LoggingJsonRecorder.class.
-	private static final Logger log = LoggerFactory.getLogger(HealthCheck.class);
-    
+	// private static final Logger log = LoggerFactory.getLogger(HealthCheck.class);
+
+  private static final Logger logger = Logger.getLogger(MyClass.class.getName());
+
+
 	@Override
     public HealthCheckResponse call() {
-        log.info("Test log of structured arg");
+        logger.info("Test log of structured arg");
         return HealthCheckResponse.up("coffee-shop");
 
     }
